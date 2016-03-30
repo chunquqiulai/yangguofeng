d81bc36c369081aa86441a16623a0f6c4636e872
yangguofeng <514074752@qq.com>
2016-03-30 16:11:04 +0800

    m dbtest4

--- test.php
+++ test.php
@@ -1,8 +1,27 @@
 <?php
 echo 23456;
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$serverName =   env("MYSQL_PORT_3306_TCP_ADDR", "localhost");
+$databaseName = env("MYSQL_INSTANCE_NAME", "homestead");
+$username =     env("MYSQL_USERNAME", "homestead");
+$password =     env("MYSQL_PASSWORD", "secret");
 
+/**
+ * 获取环境变量
+ * @param $key
+ * @param null $default
+ * @return null|string
+ */
+function env($key, $default = null)
+{
+    $value = getenv($key);
+    if ($value === false) {
+        return $default;
+    }
+    return $value;
+}
+
+var_dump($serverName);
+
+$pdo = new PDO($serverName, $username, $password);
+var_dump($pdo);
+echo 'end';
